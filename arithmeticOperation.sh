8b816eee06eeac150372c719fc6d56bccb755228
icode32 <www.gautam971997@gmail.com>
2020-11-05 00:17:37 +0530

    Store_In_Dictionary

--- arithmeticOperation.sh
+++ arithmeticOperation.sh
@@ -10,3 +10,8 @@ add_mul=$(($a+$b*$c));
 mul_add=$(($a*$b+$c));
 add_divide=$(($c+$a/$b));
 modulo_add=$(($a%$b+$c));
+
+A_Operation[add_mul]=$add_mul;
+A_Operation[mul_add]=$mul_add;
+A_operation[add_divide]=$add_divide;
+A_Operation[modulo_add]=$modulo_add;
